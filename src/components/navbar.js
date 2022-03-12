012a01e39e6e6f44d348861ff8c36ec80bab93cb
sario-tedesco <sariotedesco@gmail.com>
2022-03-12 21:27:47 +0100

    Updates

--- src/components/navbar.js
+++
@@ -1,9 +0,0 @@
-export default function Navbar(){
-    return(
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1">IV-VisualEditor</span>
-      </div>
-    </nav>
-    );
-}
