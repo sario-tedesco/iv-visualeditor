31821b1cc613775955fb0c9be20ea54f911721ed
sario-tedesco <sariotedesco@gmail.com>
2022-03-12 20:34:03 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import Navbar from './components/navbar.js';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-      <Navbar />
+      //<Navbar />
+      <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">IV-VisualEditor</span>
+      </div>
+    </nav>
   );
 }
 
